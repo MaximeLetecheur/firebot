52826c66ce28b130a1536570b391c7fe83d12723
Romain Marbaix <contact@romainmarbaix.be>
2019-02-23 00:27:33 +0100

    style: if/else block on multiple lignes

--- src/commands/list.js
+++ src/commands/list.js
@@ -2,6 +2,10 @@ module.exports = (bot, msg, args) => {
 	if (!msg.guild) return;
 
 	const titles = bot.songQueue.queue.map(track => `Title: ${track.title}, Requested by : ${track.requestor}`);
-	if (titles.length == 0) {msg.channel.send('There are no songs in the queue.');}
-	else {msg.channel.send(titles.join('\n'));}
+	if (titles.length == 0) {
+		msg.channel.send('There are no songs in the queue.');
+	}
+	else {
+		msg.channel.send(titles.join('\n'));
+	}
 };
