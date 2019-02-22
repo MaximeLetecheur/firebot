e759c729f42b4fb5efe27e77d9d5a5013a6d98ea
Romain Marbaix <contact@romainmarbaix.be>
2019-02-23 00:39:07 +0100

    fix: Use argument for purge command

--- src/commands/purge.js
+++ src/commands/purge.js
@@ -1,6 +1,19 @@
 module.exports = (bot, msg, args) => {
 	if (!msg.guild) return;
 
+	if (args.length > 1) {
+		msg.channel.send(':x: Number of arguments incorrect.');
+		console.log(':x: Number of arguments incorrect.');
+		return;
+	}
+
+	const limit = args.length == 1 ? parseInt(args[0]) : 100;
+	if (isNaN(limit)) {
+		msg.channel.send(':x: The argument is not a number.');
+		console.log(':x: The argument is not a number.');
+		return;
+	}
+
 	if (!msg.channel.permissionsFor(msg.author).has('MANAGE_MESSAGES')) {
 		msg.channel.send('Sorry, you don\'t have the permission to execute the command ' + msg.content);
 		console.log('Sorry, you don\'t have the permission to execute the command ' + msg.content);
@@ -9,20 +22,6 @@ module.exports = (bot, msg, args) => {
 	else if (!msg.channel.permissionsFor(bot.discordClient.user).has('MANAGE_MESSAGES')) {
 		msg.channel.send('Sorry, I don\'t have the permission to execute the command ' + msg.content);
 		console.log('Sorry, I don\'t have the permission to execute the command ' + msg.content);
-		return;
-	}
-
-	const parts = msg.content.split(' ');
-	if (parts.length > 2) {
-		msg.channel.send('Number of arguments incorrect.');
-		console.log('Number of arguments incorrect.');
-		return;
-	}
-
-	const limit = parts.length == 2 ? parseInt(msg.content.split(' ')[1]) : 100;
-	if (isNaN(limit)) {
-		msg.channel.send('The argument is not a number.');
-		console.log('The argument is not a number.');
 		return;
 	}
 
