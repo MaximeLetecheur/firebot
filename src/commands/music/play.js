b95a97a190ef11cae005e2e41ae74a0a0f76252e
Romain Marbaix <contact@romainmarbaix.be>
2019-02-23 14:52:08 +0100

    refactor: Remove utils.js and parseMessage method

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -1,12 +1,9 @@
-const parseMessage = require('../../utils').parseMessage;
 const youtube = require('ytdl-core');
 
 module.exports = (bot, msg, args) => {
 	if (!msg.guild) return;
 
-	const parts = parseMessage(msg.content);
-
-	const youtubeURL = youtube.validateURL(parts.content) ? parts.content : null;
+	const youtubeURL = youtube.validateURL(args[0]) ? args[0] : null;
 	if (youtubeURL == null) {
 		msg.channel.send(':x: This is not a valid Youtube URL.');
 		return;
@@ -19,7 +16,7 @@ module.exports = (bot, msg, args) => {
 		if (info.length_seconds <= TEN_MINUTES) {
 			bot.songQueue.add({
 				title: info.title,
-				search: parts.content,
+				search: args[0],
 				requestor: msg.author.username,
 				youtubeURL: youtubeURL,
 				info: info,
