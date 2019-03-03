d513f36f1835aee0407b5f20c52f2742846ee4bd
Rominou <befire@hotmail.fr>
2019-03-03 01:55:44 +0100

    code: Improve ping command

--- src/commands/utils/ping.js
+++ src/commands/utils/ping.js
@@ -1,5 +1,32 @@
-exports.exec = (bot, msg) => {
-	msg.channel.send('pong!');
+const avrArray = require('../../methods/avrArray');
+
+exports.exec = async function(bot, msg) {
+	const pongMessage = await msg.channel.send('Pinging...');
+
+	const fields = [{
+		name: 'Response time',
+		value: parseInt(pongMessage.createdTimestamp) - parseInt(msg.createdTimestamp) + ' ms',
+		inline: true,
+	}];
+	if (msg.guild.id in bot.voiceConnections) {
+		fields.push({
+			name: 'WebSocket ping',
+			value: avrArray(bot.voiceConnections[msg.guild.id].sockets.ws.client.pings) + ' ms',
+			inline: true,
+		});
+	}
+
+	msg.channel.send({
+		embed: {
+			title: '__Ping result__',
+			fields: fields,
+			timestamp: new Date(),
+		},
+	});
+
+	if (pongMessage.deletable && !pongMessage.deleted) {
+		pongMessage.delete();
+	}
 };
 
 exports.config = {
