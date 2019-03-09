01bf77aec58837843fbcdb6a617df9b2e2bc71fe
Maxime Letecheur <etu34041@student.henallux.be>
2019-03-10 00:38:15 +0100

    anime command with kitsu api + date refactoring

---
+++ src/services/kitsu-api.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+const apiUrl = 'https://kitsu.io/api/edge/';
+const anime = 'anime/';
+
+function KitsuAPI() {
+    // Constructor of KistuAPI Object
+}
+
+KitsuAPI.prototype.findByAnimeName = function(animeName) {
+    
+	return axios.get(`${apiUrl}${anime}?filter%5Btext%5D=${animeName}`);
+};
+
+module.exports = KitsuAPI;
