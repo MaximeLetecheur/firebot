ef33aa92631c9e0e71de91e30d0556b702f597d4
Rominou <befire@hotmail.fr>
2019-03-11 00:10:29 +0100

    style: Avatar command

--- src/commands/utils/avatar.js
+++ src/commands/utils/avatar.js
@@ -1,10 +1,10 @@
 exports.exec = (bot, msg) => {
-    let user = msg.author;
-    if (msg.mentions && msg.mentions.users && msg.mentions.users.array().length > 0) {
-        user = msg.mentions.users.first();
-    }
+	let user = msg.author;
+	if (msg.mentions && msg.mentions.users && msg.mentions.users.array().length > 0) {
+		user = msg.mentions.users.first();
+	}
 
-    msg.channel.send(user.displayAvatarURL);
+	msg.channel.send(`Here is the avatar of ${user} : ${user.displayAvatarURL}`);
 };
 
 exports.config = {
